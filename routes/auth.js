164889ea46700b845eaa85433b88d0225b9ae838
Dominic Bruno <dominicbtuno@sbcglobal.net>
2021-04-17 09:35:27 -0500

    Auth Route issue && Layout HBS issues || CONFRIM GH ENV VARs

--- routes/auth.js
+++ routes/auth.js
@@ -13,7 +13,110 @@ router.get('/login', (req, res) => {
 
 // Login - Check in DB
 router.post('/login', async (req, res) => {
-    const { email, password } = req.body;
+    const { email, password } = req.body;const express = require('express');
+    const router = express.Router();
+    const User = require('../models/User');
+    const bcrypt = require('bcryptjs');
+    const passport = require('passport');
+    
+    // Login - View
+    router.get('/login', (req, res) => {
+        res.render('login', {
+            layout: 'login'
+        });
+    });
+    
+    // Login - Check in DB
+    router.post('/login', async (req, res) => {
+        const { email, password } = req.body;
+    
+        const existingUser = await User.findOne({where: { email: email } });
+    
+        // Find if User exists in system
+        if(existingUser) {
+    
+            // Password Authentication
+            if(await bcrypt.compare(password, existingUser.password)) {
+              
+                // Authorize User to the Session & Pass User details
+                req.session.authorizedUser = {
+                    auth: true,
+                    displayName: existingUser.displayName,
+                    createdAt: existingUser.createdAt
+                }
+    
+                const userID = existingUser.id;
+                // Passport function call 
+                req.login(existingUser, function(err) {
+                    if(err) {
+                        console.log("UT OH");
+                    }
+                });
+    
+                res.redirect('/home');
+            } else {
+                return res.render('errors/wrong-password', {
+                    layout: 'login'
+                });
+            }
+            
+        } else {
+            return res.render('errors/user-does-not-exist', {
+                layout: 'login'
+            });
+        }
+    });
+    
+    // Initialiaze Persistance of Session
+    passport.serializeUser(function(userID, done) {
+        done(null, userID);
+    });
+      
+    passport.deserializeUser(function(userID, done) {
+        done(null, userID);
+    });
+    
+    
+    // Register View 
+    router.get('/register', (req, res) => {
+        res.render('register', {
+            layout: 'login'
+        });
+    });
+    
+    // Register - Add in DB
+    router.post('/register', async (req, res) => {
+        const { displayName, email, password } = req.body;
+        
+         // Find if User already exists in system
+        let existingUser = await User.findOne({where: { email: email }});
+    
+        if(existingUser) {
+            return res.render('errors/user-already-exists', {
+                layout: 'login'
+            });
+        } else {
+    
+            const hashedPassword = await bcrypt.hash(password, 10);
+            // Create  new User obj for DB w/ hashedPassword if User doesn't exist
+            const newUser = new User({
+                displayName, 
+                email,
+                password: hashedPassword
+            }); 
+    
+            await newUser.save();
+    
+            res.redirect('/auth/login');
+        }
+    });
+    
+    router.get('/logout', (req, res) => {
+        req.session.destroy();
+        res.redirect('/');
+    });
+    
+    module.exports = router;
 
     const existingUser = await User.findOne({where: { email: email } });
 
@@ -40,13 +143,16 @@ router.post('/login', async (req, res) => {
 
             res.redirect('/home');
         } else {
-            return res.render('errors/wrong-password');
+            return res.render('errors/wrong-password', {
+                layout: 'login'
+            });
         }
         
     } else {
-        return res.render('errors/user-does-not-exist');
+        return res.render('errors/user-does-not-exist', {
+            layout: 'login'
+        });
     }
-});
 
 // Initialiaze Persistance of Session
 passport.serializeUser(function(userID, done) {
@@ -61,7 +167,7 @@ passport.deserializeUser(function(userID, done) {
 // Register View 
 router.get('/register', (req, res) => {
     res.render('register', {
-        layout: login
+        layout: 'login'
     });
 });
 
@@ -73,7 +179,9 @@ router.post('/register', async (req, res) => {
     let existingUser = await User.findOne({where: { email: email }});
 
     if(existingUser) {
-        return res.render('errors/user-already-exists');
+        return res.render('errors/user-already-exists', {
+            layout: 'login'
+        });
     } else {
 
         const hashedPassword = await bcrypt.hash(password, 10);
